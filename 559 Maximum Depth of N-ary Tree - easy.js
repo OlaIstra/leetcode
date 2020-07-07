a5482cdcc4b3c5e5db2dd392311bf776b28f28a7
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-07 09:59:33 +0300

    Create 559 Maximum Depth of N-ary Tree - easy.js

---
+++ 559 Maximum Depth of N-ary Tree - easy.js
@@ -0,0 +1,23 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+
+    if(!root) return 0;
+    
+    let depth = 0;
+    for(let child of root.children) {
+       depth = Math.max(depth, maxDepth(child));
+    }
+    
+    return depth+1;
+}
