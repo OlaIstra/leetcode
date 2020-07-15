0c377d540ac9dcba0d9a2cb6607790ab9cd5395a
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-15 08:09:00 +0300

    Create 806 Number of Lines To Write String - lookThrough.js

---
+++ 806 Number of Lines To Write String - lookThrough.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} widths
+ * @param {string} S
+ * @return {number[]}
+ */
+const numberOfLines = function(widths, S) {
+    let lines = 1;
+  let last = 0;
+  const base = 'a'.charCodeAt(0);
+  for (let i = 0; i < S.length; i++) {
+    const width = widths[S.charCodeAt(i) - base];
+    last += width;
+    if (last > 100) {
+      lines++;
+      last = width;
+    }
+  }
+  return [lines, last];
+};
