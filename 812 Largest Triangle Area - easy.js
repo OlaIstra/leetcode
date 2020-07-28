b1d5066c2a51cf20d7417a3b1d460cec26cfbb42
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-28 15:26:41 +0200

    Create 812 Largest Triangle Area - easy.js

---
+++ 812 Largest Triangle Area - easy.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+const largestTriangleArea = function(points) {
+    let maxArea = 0;
+    
+    for (let i = 0; i < points.length; i++) {
+        for (let j = 1; j < points.length; j++) {
+            for (let k = 2; k < points.length; k++) {
+                if (i !== j && i !== k && j !== k) {
+                maxArea = Math.max(getArea(points[i], points[j], points[k]), maxArea);
+                }
+            }
+        }
+    }
+    
+    return maxArea;
+};
+
+const getArea = ([x1, y1], [x2, y2], [x3, y3]) => {
+    return Math.abs(((x2 - x1)*(y3 - y1)) - ((x3 - x1)*(y2 - y1))) / 2;
+};
