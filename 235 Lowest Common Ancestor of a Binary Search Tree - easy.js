56d573cd5dc0abfcbb7c1f08d873701d2ef413c5
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-07-28 15:38:10 +0200

    Create 235 Lowest Common Ancestor of a Binary Search Tree - easy.js

---
+++ 235 Lowest Common Ancestor of a Binary Search Tree - easy.js
@@ -0,0 +1,22 @@
+// Iterative solution
+// Time Complexity: O(H)
+// Space Complexity: O(1)
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function(root, p, q) {
+    while (root) {
+        if (root.val < p.val && root.val < q.val) {
+            root = root.right;
+        }
+        else if (root.val > p.val && root.val > q.val) {
+            root = root.left;
+        } else {
+            break;
+        }
+    }
+    return root;
+};
