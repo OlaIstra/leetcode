48d15df64d22f9e884ad6351ae27c1a046fcf285
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-08-02 22:48:57 +0300

    Create 447 Number of Boomerangs - easy.js

---
+++ 447 Number of Boomerangs - easy.js
@@ -0,0 +1,19 @@
+const numberOfBoomerangs = function(points) {
+    let count = 0;
+    
+    for (let i = 0; i < points.length; i++) {
+        const memory = {};
+
+        for (let j = 0; j < points.length; j++) {
+            if (i === j) continue;
+            
+            // search points with identical distance in memory and count tuples
+            const dist = Math.pow(points[i][0] - points[j][0],2) + Math.pow(points[i][1] - points[j][1],2);
+            if (memory[dist]) count += memory[dist] * 2;
+            
+            memory[dist] ? memory[dist] += 1 : memory[dist] = 1;
+        }
+    }
+    
+    return count;
+};
