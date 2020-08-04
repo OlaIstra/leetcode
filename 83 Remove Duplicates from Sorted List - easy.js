4b7410c547cb550b59ac2bcf8fc6a31d49a47d8a
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-08-04 08:32:53 +0300

    Create 83 Remove Duplicates from Sorted List - easy.js

---
+++ 83 Remove Duplicates from Sorted List - easy.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function(head) {
+    let current = head
+    while (current && current.next) {
+        current.val === current.next.val
+            ? current.next = current.next.next
+            : current = current.next
+    }
+    return head
+};
