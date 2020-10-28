abcfc57f145505aec4a756c926422e0f59a22a6d
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-10-28 21:13:23 +0100

    Create 278 First Bad Version - easy.js

---
+++ 278 First Bad Version - easy.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let left = 0
+        let right = n
+        
+        while (right - left !== 1) {
+            const middle = Math.ceil((left + right) / 2)
+            if (isBadVersion(middle)) {
+                right = middle
+            } else {
+                left = middle
+            }
+        }
+        
+        return right
+    };
+};
