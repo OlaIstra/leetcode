421dacd121ca8cf39402c8ebba95f37732e5ee78
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-10-28 20:52:02 +0100

    Create 58 Length of Last Word - easy.js

---
+++ 58 Length of Last Word - easy.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function(s) {
+    const arr = s.split(' ')
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] !== '') {
+            return arr[i].length
+        }
+    }
+    
+    return 0
+};
