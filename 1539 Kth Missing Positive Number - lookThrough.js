eb80ef9d0d34391ea47d696a353bcd533b13a736
olga.ibragimova <38282991+OlaIstra@users.noreply.github.com>
2020-11-22 18:30:11 +0100

    Create 1539 Kth Missing Positive Number - lookThrough.js

---
+++ 1539 Kth Missing Positive Number - lookThrough.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+const findKthPositive = function(arr, k) {
+    for (let i = 1, j = 0; ; i++) {
+        if (i === arr[j]) {
+            j++
+        }
+        else if (!--k) {
+            return i;
+        }
+    }    
+};
